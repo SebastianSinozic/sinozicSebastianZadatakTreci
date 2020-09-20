bccd7f8e985a502b3f4860e23a000129ee619d3a
SebastianSinozic <55134663+SebastianSinozic@users.noreply.github.com>
2020-09-20 21:37:57 +0200

    Add files via upload

---
+++ sinozicSebastianZadatakTreci/sinozicSebastianZadatakTreci/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace sinozicSebastianZadatakTreci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Br = Convert.ToInt32(Console.ReadLine());
+
+            if (Br % 4 == 0)
+            {
+                Console.WriteLine(" Broj je djeljiv sa 4. ");
+            }
+            if (Br % 6 == 0)
+            {
+         Console.WriteLine("Upisali ste broj: ", Br);
+         Console.WriteLine("Broj je djeljiv sa 6.");
+             }
+        if (Br % 4 == 0 && Br % 6 == 0)
+             {
+         Console.WriteLine("Upisali ste broj: ", Br);
+         Console.WriteLine("Broj je djeljiv sa 4 i sa 6.");
+             }
+         if (Br % 4 != 0 && Br % 6 != 0)
+             {
+         Console.WriteLine("Upisali ste broj: ", Br);
+         Console.WriteLine("Broj nije djeljiv sa 4 i sa 6.");
+             }
+         Console.ReadKey();
+  }
+ }
+}
